7103d83671c146ccc6bb80468742f8788793e4b0
Misael Esperanzate <misael.esperanzate@hotmail.com>
2021-10-05 21:08:42 -0600

    1

---
+++ sorting-visualizer/src/utils/GenerateValues.js
@@ -0,0 +1,10 @@
+const generateRandomValues = ({ arraySize }) => {
+	let randomizedArray = [];
+
+	for (let i = 0; i < arraySize; i++) {
+		randomizedArray.push(Math.floor(Math.random() * (300 - 10) + 10));
+	}
+	return randomizedArray;
+};
+
+export default generateRandomValues;
